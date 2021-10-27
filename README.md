14bd219bcce6d495ec292639899395137540165d
Axel Mukwena <axel.muk@gmail.com>
2021-10-28 04:06:38 +0800

    Update codebase

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
-# covidVisuals
+# Covid-19 Visualization around Namibia
  Developing automatic dashboards which allows users to query and customize visualisations with dash and flask
+
+## Running the program
+#### NB: Any line that starts with `$` indicates that the following text should be executed in the terminal/CMD prompt.
+
+- Open up the Terminal or CMD, navigate to the `covidVisuals` directory location (replace <ins>folder/folder</ins> with the correct location) by...
+
+      $ cd folder/folder/covidVisuals
+      
+- Install pip3
+
+      # Linux
+      $ sudo apt-get install python3-pip
+
+- Install virtualenv
+      
+      # Linux
+      $ sudo apt-get install virtualenv
+      
+      # Mac OS
+      $ sudo -H pip install virtualenv
+      
+      # Windows
+      $ pip install virtualenv
+  
+- Create a virtual environment
+  
+      # Mac OS / Linux
+      $ virtualenv -p python3 venv
+
+      # Windows
+      $ python3 -mvenv venv
+      
+- Activate the environment
+  
+      # Mac OS / Linux
+      $ source venv/bin/activate
+      
+      # Windows
+      $ venv\Scripts\activate
+
+- Install the requirements.txt
+
+      # openpyxl is a dependency for Pandas
+      $ pip install openpyxl
+  
+      # Mac OS / Linux
+      $ pip3 install -r requirements.txt
+      
+      # Windows
+      $ python3 -m pip install -r requirements.txt
+    
+- Run the script...
+
+      $ python3 main.py
+
+
+### Installing or updating packages
+
+- Go to folder
+
+      $ cd folder/folder/covidVisuals
+
+- Activate env
+
+      # Mac OS
+      $ source venv/bin/activate
+
+      # Windows
+      $ venv\Scripts\activate
+  
+- Install or update package
+
+      # specific command
+
+- Update *requirements.txt*
+
+      $ pip freeze > requirements.txt
+
+ 
+
--- main.py
+++ main.py
@@ -1,16 +1,146 @@
-# This is a sample Python script.
+import os
+import dash
+from dash import html
+from dash import dcc
+import plotly.graph_objects as go
+import pandas as pd
+from dash.dependencies import Input, Output
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+country = None
+regions = {}
+region_names = []
+column_names = []
+# Initialize app
+app = dash.Dash()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# `end_date - start_date` outputs a stamp e.g `305 days 12:00:00`
+# If we convert that to a string, split by spaces, get the first index
+# and convert it to int, we get the number of valid days.
+# This is because, e.g the 2020 data file contains extra 2021 rows which
+# just messes with the whole data structure, thus with valid rows,
+# we get the accurate data rows for the year.
+# + 1 because end of the year date completes at the start of the year
+# or just make `end_date = beginning of the year`
+def valid_rows(start_date):
+    year = str(start_date)[:4]
+    end_date = pd.Timestamp(year + '-12-31')
+    rows = int(str(end_date - start_date).split()[0]) + 1
+    return rows
 
 
-# Press the green button in the gutter to run the script.
+def country_wide():
+    global country
+    global regions
+
+    for region in regions:
+        df = regions[region]
+        # Initial stage
+        if country is None:
+            country = df
+        else:
+            put_date_aside = df['Date']
+
+            # Drop date columns because they cannot be added, only numbers
+            df = df.drop('Date', 1)
+            country = country.drop('Date', 1)
+
+            # Combine df with country dataframes
+            country = country.add(df, fill_value=0)
+            country.insert(0, 'Date', put_date_aside)
+
+            temp = country
+            print()
+
+
+def region_wide(sheet, df):
+    region_names.append(sheet)
+    columns = df.keys()
+
+    # Rename columns to clean names
+    for c in columns:
+        column = c.strip()
+        column_names.append(column)
+        df = df.rename(columns={c: c.strip()})
+
+    # Trim dataframe to valid rows
+    start_date = df[column_names[0]][0]
+    rows = valid_rows(start_date)
+    df = df[:rows]
+
+    if sheet in regions:
+        regions[sheet] = regions[sheet].append(df, ignore_index=True)
+    else:
+        regions[sheet] = df
+
+
+# Read the data from Excel files
+def import_data(root):
+    files = sorted(os.listdir(root))
+    for file in files:
+        # Just in case you open an excel file for viewing and never close it,
+        # Excel create a temporary file starting with `~$`. Thus exclude such
+        # files just in case they exist
+        if file.endswith('.xlsx') and not file.startswith('~$'):
+            filename = os.path.join(root, file)
+            sheets = pd.read_excel(filename, sheet_name=None).keys()
+            for sheet in sheets:
+                df = pd.read_excel(filename, index_col=None, header=0, sheet_name=sheet)
+                # Process data into regions
+                if sheet == 'Karas':
+                    sheet = 'Kharas'
+                region_wide(sheet, df)
+
+
+# Main driver program
+def main():
+    import_data('dataset')
+    # Process data into country
+    country_wide()
+
+
+# Initialize app layouts
+def app_layouts():
+    app.layout = html.Div(id='parent', children=[
+        html.H1(id='H1', children='Covid-19 Cases',
+                style={'textAlign': 'center', 'marginTop': 40, 'marginBottom': 40}),
+
+        dcc.Dropdown(id='dropdown',
+                     options=[
+                         {'label': column_names[1], 'value': column_names[1]},
+                         {'label': column_names[2], 'value': column_names[2]},
+                         {'label': column_names[3], 'value': column_names[3]},
+                         {'label': column_names[4], 'value': column_names[4]},
+                         {'label': column_names[5], 'value': column_names[5]},
+                         {'label': column_names[6], 'value': column_names[6]},
+                         {'label': column_names[7], 'value': column_names[7]},
+                         {'label': column_names[8], 'value': column_names[8]},
+                     ],
+                     value=column_names[1]),
+                     # multi=True),
+        dcc.Graph(id='bar_plot')
+    ])
+
+
+# Server request functions
+@app.callback(Output(component_id='bar_plot', component_property='figure'),
+              [Input(component_id='dropdown', component_property='value')])
+def graph_update(dropdown_value):
+    print(dropdown_value)
+    fig = go.Figure([go.Scatter(x=country['Date'], y=country['{}'.format(dropdown_value)],
+                                line=dict(color='firebrick', width=4))
+                     ])
+
+    fig.update_layout(title='Covid cases in the entire country',
+                      xaxis_title='Dates',
+                      yaxis_title='Cases'
+                      )
+    return fig
+
+
+# Initializer
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Load data, initialize layouts, run server
+    main()
+    app_layouts()
+    app.run_server()
