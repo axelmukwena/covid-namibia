aeeecbabb6b6748663d2cf56f9de6784fb4510c8
Axel Mukwena <axel.muk@gmail.com>
2021-10-29 02:32:27 +0800

    Add favicon

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ external_stylesheets = [
     {
         "href": "https://fonts.googleapis.com/css2?"
                 "family=Lato:wght@400;700&display=swap",
-        # "rel": "stylesheet",
+        "rel": "stylesheet",
     },
 ]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
