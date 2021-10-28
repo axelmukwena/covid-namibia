265319c0f887533bf2737bc46af53c373d3771ca
Axel Mukwena <axel.muk@gmail.com>
2021-10-28 17:21:33 +0800

    Updates

--- main.py
+++ main.py
@@ -5,119 +5,45 @@ from dash import dcc
 import plotly.graph_objects as go
 import pandas as pd
 from dash.dependencies import Input, Output
+import process
 
-country = None
 regions = {}
-region_names = []
 column_names = []
 # Initialize app
 app = dash.Dash()
 
 
-# `end_date - start_date` outputs a stamp e.g `305 days 12:00:00`
-# If we convert that to a string, split by spaces, get the first index
-# and convert it to int, we get the number of valid days.
-# This is because, e.g the 2020 data file contains extra 2021 rows which
-# just messes with the whole data structure, thus with valid rows,
-# we get the accurate data rows for the year.
-# + 1 because end of the year date completes at the start of the year
-# or just make `end_date = beginning of the year`
-def valid_rows(start_date):
-    year = str(start_date)[:4]
-    end_date = pd.Timestamp(year + '-12-31')
-    rows = int(str(end_date - start_date).split()[0]) + 1
-    return rows
-
-
-def country_wide():
-    global country
-    global regions
-
-    for region in regions:
-        df = regions[region]
-        # Initial stage
-        if country is None:
-            country = df
-        else:
-            put_date_aside = df['Date']
-
-            # Drop date columns because they cannot be added, only numbers
-            df = df.drop('Date', 1)
-            country = country.drop('Date', 1)
-
-            # Combine df with country dataframes
-            country = country.add(df, fill_value=0)
-            country.insert(0, 'Date', put_date_aside)
-
-            temp = country
-            print()
-
-
-def region_wide(sheet, df):
-    region_names.append(sheet)
-    columns = df.keys()
-
-    # Rename columns to clean names
-    for c in columns:
-        column = c.strip()
-        column_names.append(column)
-        df = df.rename(columns={c: c.strip()})
-
-    # Trim dataframe to valid rows
-    start_date = df[column_names[0]][0]
-    rows = valid_rows(start_date)
-    df = df[:rows]
-
-    if sheet in regions:
-        regions[sheet] = regions[sheet].append(df, ignore_index=True)
-    else:
-        regions[sheet] = df
-
-
-# Read the data from Excel files
-def import_data(root):
-    files = sorted(os.listdir(root))
-    for file in files:
-        # Just in case you open an excel file for viewing and never close it,
-        # Excel create a temporary file starting with `~$`. Thus exclude such
-        # files just in case they exist
-        if file.endswith('.xlsx') and not file.startswith('~$'):
-            filename = os.path.join(root, file)
-            sheets = pd.read_excel(filename, sheet_name=None).keys()
-            for sheet in sheets:
-                df = pd.read_excel(filename, index_col=None, header=0, sheet_name=sheet)
-                # Process data into regions
-                if sheet == 'Karas':
-                    sheet = 'Kharas'
-                region_wide(sheet, df)
-
-
 # Main driver program
 def main():
-    import_data('dataset')
-    # Process data into country
-    country_wide()
+    global regions, column_names
+
+    process.data()
+    regions = process.regions
+    column_names = process.column_names
+
+
+external_stylesheets = [
+    {
+        "href": "https://fonts.googleapis.com/css2?"
+        "family=Lato:wght@400;700&display=swap",
+        "rel": "stylesheet",
+    },
+]
 
 
 # Initialize app layouts
 def app_layouts():
+    app.title = "Covid-19 Cases | Namibia"
+
     app.layout = html.Div(id='parent', children=[
         html.H1(id='H1', children='Covid-19 Cases',
                 style={'textAlign': 'center', 'marginTop': 40, 'marginBottom': 40}),
 
         dcc.Dropdown(id='dropdown',
-                     options=[
-                         {'label': column_names[1], 'value': column_names[1]},
-                         {'label': column_names[2], 'value': column_names[2]},
-                         {'label': column_names[3], 'value': column_names[3]},
-                         {'label': column_names[4], 'value': column_names[4]},
-                         {'label': column_names[5], 'value': column_names[5]},
-                         {'label': column_names[6], 'value': column_names[6]},
-                         {'label': column_names[7], 'value': column_names[7]},
-                         {'label': column_names[8], 'value': column_names[8]},
-                     ],
-                     value=column_names[1]),
-                     # multi=True),
+                     options=[{'label': k, 'value': k} for k in column_names[1:]],
+                     value=column_names[1],
+                     # multi=True
+                     ),
         dcc.Graph(id='bar_plot')
     ])
 
@@ -127,11 +53,12 @@ def app_layouts():
               [Input(component_id='dropdown', component_property='value')])
 def graph_update(dropdown_value):
     print(dropdown_value)
-    fig = go.Figure([go.Scatter(x=country['Date'], y=country['{}'.format(dropdown_value)],
-                                line=dict(color='firebrick', width=4))
-                     ])
+    fig = go.Figure([go.Scatter(x=regions['Khomas']['Date'], y=regions['Khomas']['{}'.format(dropdown_value)],
+                                name=dropdown_value
+                                )])
 
-    fig.update_layout(title='Covid cases in the entire country',
+    fig.update_traces(line_shape="spline")
+    fig.update_layout(title='Covid cases in Khomas region',
                       xaxis_title='Dates',
                       yaxis_title='Cases'
                       )
---
+++ process.py
@@ -0,0 +1,105 @@
+import os
+import pandas as pd
+
+regions = {}
+column_names = []
+
+
+# `end_date - start_date` outputs a stamp e.g `305 days 12:00:00`
+# If we convert that to a string, split by spaces, get the first index
+# and convert it to int, we get the number of valid days.
+# This is because, e.g the 2020 data file contains extra 2021 rows which
+# just messes with the whole data structure, thus with valid rows,
+# we get the accurate data rows for the year.
+# + 1 because end of the year date completes at the start of the year
+# or just make `end_date = beginning of the year`
+def valid_rows(start_date):
+	year = str(start_date)[:4]
+	end_date = pd.Timestamp(year + '-12-31')
+	rows = int(str(end_date - start_date).split()[0]) + 1
+	return rows
+
+
+# Write file to csv, with specified columns
+def write_csv(destination, file, df):
+	if not os.path.isdir(destination):
+		os.makedirs(destination)
+
+	path = os.path.join(destination, file + '.csv')
+	df.to_csv(path)
+
+
+def country_wide():
+	global regions
+
+	country = None
+	for region in regions:
+		df = regions[region]
+		# Initial stage
+		if country is None:
+			country = df
+		else:
+			put_date_aside = df['Date']
+
+			# Drop date columns because they cannot be added, only numbers
+			df = df.drop('Date', 1)
+			country = country.drop('Date', 1)
+
+			# Combine df with country dataframes
+			country = country.add(df, fill_value=0)
+			country.insert(0, 'Date', put_date_aside)
+
+		country = country.dropna()
+		regions[region] = regions[region].dropna()
+		write_csv('dataset/processed/', region, regions[region])
+
+	regions['Country'] = country
+	write_csv('dataset/processed/', 'Country', regions['Country'])
+
+
+def region_wide(sheet, filename):
+	df = pd.read_excel(filename, index_col=None, header=0, sheet_name=sheet)
+	columns = df.keys()
+
+	# Rename columns to clean names
+	for c in columns:
+		column = c.strip()
+		column_names.append(column)
+		df = df.rename(columns={c: c.strip()})
+
+	# Trim dataframe to valid rows
+	start_date = df[column_names[0]][0]
+	rows = valid_rows(start_date)
+	df = df[:rows]
+
+	if sheet in regions:
+		regions[sheet] = regions[sheet].append(df, ignore_index=True)
+	else:
+		regions[sheet] = df
+
+
+# Read the data from Excel files
+def import_data(root):
+	files = sorted(os.listdir(root))
+	for file in files:
+		# Just in case you open an excel file for viewing and never close it,
+		# Excel create a temporary file starting with `~$`. Thus exclude such
+		# files just in case they exist
+		if file.endswith('.xlsx') and not file.startswith('~$'):
+			filename = os.path.join(root, file)
+			sheets = pd.read_excel(filename, sheet_name=None).keys()
+			for sheet in sheets:
+				# Process data into regions
+				if file.startswith('2020'):  # Process all of 2020 data
+					if sheet == 'Karas':
+						sheet = 'Kharas'
+					region_wide(sheet, filename)
+				elif file.startswith('2021'):
+					if sheet == 'Khomas':  # Only process one region, Khomas [2021]
+						region_wide(sheet, filename)
+
+
+def data():
+	import_data('dataset/raw/')
+	# Process data into country
+	country_wide()
